38f8792863782a763b4ca60c54792074e6589579
Oscar Andersson <matsked@dkho.org>
2019-10-19 08:02:49 +0200

    Initial commit.

---
+++ OATemplate/Controllers/templateController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OATemplate.Models;
+
+namespace OATemplate.Controllers
+{
+    public class TemplateController : Controller
+    {
+        
+        public ViewResult Index()
+        {
+            //Inital view just used to select a publication.
+            return View();
+        }
+        
+        [HttpPost]
+        public ViewResult PublicationSelected(FormCollection values)
+        {
+            Template template = new Template();
+            template.Publication = values["Publication"];
+            //View to select a template or make a new one.
+            return View(template);
+        }
+
+        [HttpPost]
+        public string TemplateSelected()
+        {
+            //Fills in a form with the pages numbers from the selected saved templated.
+            return "It was done.";
+        }
+
+        [HttpPost]
+        public ViewResult TemplatedCommited()
+        {
+            //Sends the template to the prepress-system and sets the view to reflect the success or failure of this operation.
+            return View();
+        }
+    }
+}
---
+++ OATemplate/Models/Template.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OATemplate.Models
+{
+    public class Template
+    {
+        public string Publication;
+
+        Press KBA;
+
+        internal class Press
+        {
+            Tower T11;
+            Tower T12;
+            Tower T13;
+            Tower T14;
+
+            internal class Tower
+            {
+                Cylinder Cyl1;
+                Cylinder Cyl2;
+                Cylinder Cyl3;
+                Cylinder Cyl4;
+                Cylinder Cyl5;
+                Cylinder Cyl6;
+                Cylinder Cyl7;
+                Cylinder Cyl8;
+
+                internal class Cylinder
+                {
+                    HighOrLow H;
+                    HighOrLow L;
+
+                    internal class HighOrLow
+                    {
+                        Section A;
+                        Section B;
+                        Section C;
+                        Section D;
+
+                        internal class Section
+                        {
+                            string pageNumber;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
